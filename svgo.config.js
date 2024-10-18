113abca93399c57674bba6c7c0a096a9310f20fd
Svitlana Terentyeva <114918313+sveta-terentyeva@users.noreply.github.com>
2024-10-18 14:14:54 +0300

    Create svgo.config.js

---
+++ svgo.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          removeViewBox: false,
+        },
+      },
+    },
+  ],
+};
